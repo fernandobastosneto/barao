d2b7e18ac121a86e00d9d20d8c833f79052ddc43
fernandobastosneto <fernando.bastos.neto@gmail.com>
2021-02-03 11:37:50 +0000

    dados paises

---
+++ R/comerciobr_dados_paises.R
@@ -0,0 +1,73 @@
+#' @export
+comerciobr_dados_paises <- function(pais, periodo) {
+
+  if (periodo == "anual") {
+
+    df <- comerciobr::sh1_df %>%
+      dplyr::group_by(co_ano, no_pais, path) %>%
+      dplyr::summarise(value = sum(value)) %>%
+      dplyr::group_by(co_ano, path) %>%
+      dplyr::arrange(dplyr::desc(value)) %>%
+      dplyr::mutate(rank = dplyr::row_number())
+  }
+
+  else {
+
+    df <- comerciobr::sh1_df %>%
+      dplyr::filter(co_mes <= barao::comerciobr_get_ultimomes()) %>%
+      dplyr::group_by(co_ano, no_pais, path) %>%
+      dplyr::summarise(value = sum(value)) %>%
+      dplyr::group_by(co_ano, path) %>%
+      dplyr::arrange(dplyr::desc(value)) %>%
+      dplyr::mutate(rank = dplyr::row_number())
+
+  }
+
+  # identificando a posição do pais no ranking - exp
+
+  rank_pais_exp <- df %>%
+    dplyr::filter(path == "EXP") %>%
+    dplyr::filter(no_pais == pais) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(co_ano == max(co_ano)) %>%
+    dplyr::pull(rank)
+
+  # identificando a posição do pais no ranking - imp
+
+  rank_pais_imp <- df %>%
+    dplyr::filter(path == "IMP") %>%
+    dplyr::filter(no_pais == pais) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(co_ano == max(co_ano)) %>%
+    dplyr::pull(rank)
+
+  # fazer um semi join para extrair apenas os países com os quais o país deve ser comparado
+  ## tem que ser semi join porque queremos o ranking e o path
+  ## queremos um elemento com duas características
+
+  rank_paises <- tibble::tibble(path = c(rep("EXP", 5),
+                                         rep("IMP", 5)),
+                                rank = c(rank_pais_exp,
+                                         rank_pais_exp-1,
+                                         rank_pais_exp+1,
+                                         rank_pais_exp+2,
+                                         rank_pais_exp-2,
+                                         rank_pais_imp,
+                                         rank_pais_imp-1,
+                                         rank_pais_imp+1,
+                                         rank_pais_imp+2,
+                                         rank_pais_imp-2))
+
+  get_paises <- df %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(co_ano == max(co_ano)) %>%
+    dplyr::semi_join(rank_paises, by = c("path", "rank")) %>%
+    dplyr::select(no_pais, path)
+
+  df %>%
+    dplyr::ungroup() %>%
+    dplyr::semi_join(get_paises)
+
+}
+
+
