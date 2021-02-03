1439ea90562362d14af28b9e6555b5682c37fd75
fernandobastosneto <fernando.bastos.neto@gmail.com>
2021-02-03 18:58:32 +0000

    correcao variacao percentual tabela paises

--- R/comerciobr_tabela_paises.R
+++ R/comerciobr_tabela_paises.R
@@ -1,16 +1,14 @@
 #' @export
 comerciobr_tabela_paises <- function(pais, periodo) {
 
-  barao::comerciobr_dados_paises(pais, periodo) %>%
-    dplyr::group_by(path) %>%
-    dplyr::arrange(no_pais) %>%
-    dplyr::group_by(no_pais) %>%
-    dplyr::mutate(pct_var = value/dplyr::lag(value)-1) %>%
-    dplyr::mutate(pct_prop = value/total) %>%
+  barao::comerciobr_dados_paises(pais, periodo)  %>%
+    dplyr::group_by(no_pais, path) %>%
+    dplyr::arrange(desc(co_ano), .by_group = T) %>%
+    dplyr::mutate(pct_var = value/dplyr::lead(value)-1) %>%
+    dplyr::mutate(pct_prop = (value/total)) %>%
     dplyr::ungroup() %>%
     dplyr::filter(co_ano >= max(co_ano)-3) %>%
     dplyr::select(-c(rank, total)) %>%
-    tidyr::drop_na() %>%
     dplyr::group_by(co_ano, path) %>%
     dplyr::arrange(desc(value), .by_group = T) %>%
     dplyr::arrange(desc(co_ano)) %>%
