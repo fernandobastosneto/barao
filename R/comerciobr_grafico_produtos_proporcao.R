355dae03aa8d9426c6d827cc1ba9ea9a1cdd4cfb
fernandobastosneto <fernando.bastos.neto@gmail.com>
2021-02-04 13:15:47 +0000

    corrigindo tamanho das células do treemap

--- R/comerciobr_grafico_produtos_proporcao.R
+++ R/comerciobr_grafico_produtos_proporcao.R
@@ -17,10 +17,8 @@ comerciobr_grafico_produtos_proporcao <- function(pais, periodo) {
     dplyr::ungroup() %>%
     dplyr::filter(co_ano == max(co_ano)) %>%
     dplyr::mutate(prop = value/total) %>%
-
-    dplyr::select(-c(value, total, rank)) %>%
     dplyr::mutate(no_sh4_por = dplyr::case_when(stringr::str_length(no_sh4_por) > 20 ~ paste0(stringr::str_sub(no_sh4_por, 1, 20), ".."),
-                                         TRUE ~ no_sh4_por)) %>%
+                                                TRUE ~ no_sh4_por)) %>%
     dplyr::group_by(co_ano, path) %>%
     dplyr::mutate(total_prop = abs(sum(prop)-1)) %>%
     dplyr::ungroup()
@@ -34,21 +32,38 @@ comerciobr_grafico_produtos_proporcao <- function(pais, periodo) {
     dplyr::distinct(total_prop) %>%
     dplyr::pull(total_prop)
 
-    df %>%
-      tibble::add_row(co_ano = ano_max,
-                      co_sh4 = "0000",
-                      path = "EXP",
-                      no_sh4_por = "Outros",
-                      prop = exp,
-                      total_prop = NA) %>%
-      tibble::add_row(co_ano = ano_max,
-                      co_sh4 = "0000",
-                      path = "IMP",
-                      no_sh4_por = "Outros",
-                      prop = imp,
-                      total_prop = NA) %>%
+  total_imp <- df %>%
+    dplyr::filter(path == "IMP") %>%
+    dplyr::distinct(total) %>%
+    dplyr::pull(total)
+
+  total_exp <- df %>%
+    dplyr::filter(path == "EXP") %>%
+    dplyr::distinct(total) %>%
+    dplyr::pull(total)
+
+
+  df %>%
+    tibble::add_row(co_ano = ano_max,
+                    co_sh4 = "0000",
+                    path = "EXP",
+                    no_sh4_por = "Outros",
+                    rank = NA,
+                    total = total_exp,
+                    value = exp*total,
+                    prop = exp,
+                    total_prop = NA) %>%
+    tibble::add_row(co_ano = ano_max,
+                    co_sh4 = "0000",
+                    path = "IMP",
+                    no_sh4_por = "Outros",
+                    rank = NA,
+                    total = total_imp,
+                    value = imp*total,
+                    prop = imp,
+                    total_prop = NA) %>%
     treemap::treemap(index = c("path", "no_sh4_por"),
-                     vSize = "prop",
+                     vSize = "value",
                      type = "index",
                      palette = ggthemes::tableau_color_pal('Tableau 10')(10),
                      title = glue::glue("Brasil-{pais}, Proporção de Exportações e Importações"))
