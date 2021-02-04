5609fa8a80cb16f54e47094f67e89c7c8ff21c58
fernandobastosneto <fernando.bastos.neto@gmail.com>
2021-02-04 13:01:03 +0000

    adicionando tabela de fatores

---
+++ R/comerciobr_tabela_fatores.R
@@ -0,0 +1,63 @@
+#' @export
+comerciobr_tabela_fatores <- function(pais, periodo, fator) {
+
+  if (fator == "cuci") {
+
+    barao::comerciobr_dados_fatores(pais, periodo, fator) %>%
+      dplyr::ungroup() %>%
+      dplyr::select(no_pais, path, no_cuci_sec, value) %>%
+      dplyr::arrange(path) %>%
+      dplyr::group_by(no_pais, path) %>%
+      dplyr::arrange(desc(value), .by_group = T) %>%
+      dplyr::mutate(dplyr::across(where(is.numeric), scales::label_number_si(accuracy = 0.1))) %>%
+      kableExtra::kbl(col.names = c("País", "Direção", "Classificação", "Valor")) %>%
+      kableExtra::collapse_rows(1:2, latex_hline = "full") %>%
+      kableExtra::add_header_above(header = setNames(4, glue::glue("{pais}-{fator}")))
+
+  }
+
+  else if (fator == "isic") {
+
+    barao::comerciobr_dados_fatores(pais, periodo, fator) %>%
+      dplyr::ungroup() %>%
+      dplyr::select(no_pais, path, no_isic_secao, value) %>%
+      dplyr::arrange(path) %>%
+      dplyr::group_by(no_pais, path) %>%
+      dplyr::arrange(desc(value), .by_group = T) %>%
+      dplyr::mutate(dplyr::across(where(is.numeric), scales::label_number_si(accuracy = 0.1))) %>%
+      kableExtra::kbl(col.names = c("País", "Direção", "Classificação", "Valor")) %>%
+      kableExtra::collapse_rows(1:2, latex_hline = "full") %>%
+      kableExtra::add_header_above(header = setNames(4, glue::glue("{pais}-{fator}")))
+
+  }
+
+  else if (fator == "cgce") {
+
+    barao::comerciobr_dados_fatores(pais, periodo, fator) %>%
+      dplyr::ungroup() %>%
+      dplyr::select(no_pais, path, no_cgce_n1, value) %>%
+      dplyr::arrange(path) %>%
+      dplyr::group_by(no_pais, path) %>%
+      dplyr::arrange(desc(value), .by_group = T) %>%
+      dplyr::mutate(dplyr::across(where(is.numeric), scales::label_number_si(accuracy = 0.1))) %>%
+      kableExtra::kbl(col.names = c("País", "Direção", "Classificação", "Valor")) %>%
+      kableExtra::collapse_rows(1:2, latex_hline = "full") %>%
+      kableExtra::add_header_above(header = setNames(4, glue::glue("{pais}-{fator}")))
+
+  }
+
+  else if (fator == "fator") {
+
+    barao::comerciobr_dados_fatores(pais, periodo, fator) %>%
+      dplyr::ungroup() %>%
+      dplyr::select(no_pais, path, no_fat_agreg, value) %>%
+      dplyr::arrange(path) %>%
+      dplyr::group_by(no_pais, path) %>%
+      dplyr::arrange(desc(value), .by_group = T) %>%
+      dplyr::mutate(dplyr::across(where(is.numeric), scales::label_number_si(accuracy = 0.1))) %>%
+      kableExtra::kbl(col.names = c("País", "Direção", "Classificação", "Valor")) %>%
+      kableExtra::collapse_rows(1:2, latex_hline = "full") %>%
+      kableExtra::add_header_above(header = setNames(4, glue::glue("{pais}-{fator}")))
+
+  }
+}
