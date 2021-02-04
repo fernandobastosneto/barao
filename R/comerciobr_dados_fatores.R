267c202dc67d001d7175ce9bd1a01dd54ea9e225
fernandobastosneto <fernando.bastos.neto@gmail.com>
2021-02-04 03:01:47 +0000

    dados fatores

---
+++ R/comerciobr_dados_fatores.R
@@ -0,0 +1,76 @@
+comerciobr_dados_fatores <- function(pais, periodo, fator) {
+
+  if (fator == "isic") {
+    isic <- comerciobr::dic_ncm_isic %>%
+      dplyr::select(-co_ncm) %>%
+      dplyr::distinct()
+
+    df <- comerciobr::isic_df %>%
+      dplyr::filter(co_ano == max(co_ano)) %>%
+      dplyr::group_by(co_isic_secao, co_pais) %>%
+      dplyr::summarise(value = sum(value)) %>%
+      dplyr::rename(CO_PAIS = co_pais) %>%
+      dplyr::left_join(comerciobr::dic_paises, by = "CO_PAIS") %>%
+      dplyr::filter(NO_PAIS == pais) %>%
+      dplyr::left_join(isic)
+
+  }
+
+  else if (fator == "cuci") {
+    cuci <- comerciobr::dic_ncm_cuci %>%
+      dplyr::select(-co_ncm) %>%
+      dplyr::distinct()
+
+    df <- comerciobr::cuci_df %>%
+      dplyr::group_by(co_ano, co_cuci_sec, co_pais) %>%
+      dplyr::summarise(value = sum(value)) %>%
+      dplyr::rename(CO_PAIS = co_pais) %>%
+      dplyr::left_join(comerciobr::dic_paises, by = "CO_PAIS") %>%
+      dplyr::filter(NO_PAIS == pais) %>%
+      dplyr::left_join(cuci) %>%
+      dplyr::filter(co_ano == max(co_ano))
+
+  }
+
+  else if (fator == "fator") {
+
+    fator <- comerciobr::dic_ncm_fator %>%
+      dplyr::select(-co_ncm) %>%
+      dplyr::distinct()
+
+    df <- comerciobr::fator_df %>%
+      dplyr::filter(co_ano == max(co_ano)) %>%
+      dplyr::group_by(co_fat_agreg, co_pais) %>%
+      dplyr::summarise(value = sum(value)) %>%
+      dplyr::rename(CO_PAIS = co_pais) %>%
+      dplyr::left_join(comerciobr::dic_paises, by = "CO_PAIS") %>%
+      dplyr::filter(NO_PAIS == pais) %>%
+      dplyr::left_join(fator)
+
+  }
+
+  else if (fator == "cgce") {
+
+    cgce <- comerciobr::dic_ncm_cgce %>%
+      dplyr::select(-co_ncm) %>%
+      dplyr::distinct() %>%
+      dplyr::mutate(co_cgce_n1 = as.integer(co_cgce_n1))
+
+    df <- comerciobr::cgce_df %>%
+      dplyr::filter(co_ano == max(co_ano)) %>%
+      dplyr::group_by(co_cgce_n1, co_pais) %>%
+      dplyr::summarise(value = sum(value)) %>%
+      dplyr::rename(CO_PAIS = co_pais) %>%
+      dplyr::left_join(comerciobr::dic_paises, by = "CO_PAIS") %>%
+      dplyr::filter(NO_PAIS == pais) %>%
+      dplyr::left_join(cgce, by = "co_cgce_n1")
+
+  }
+
+  df
+}
+
+#
+#   dplyr::group_by(co_ano, co_cuci_sec, co_pais) %>%
+#   dplyr::summarise(value = sum(value)) %>%
+#
