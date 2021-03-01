c3d4c5e1b010827374f4afc2dae552a1525d2312
fernandobastosneto <fernando.bastos.neto@gmail.com>
2021-03-01 13:54:10 -0300

    criando função para relatório

---
+++ R/comerciobr_report_pais.R
@@ -0,0 +1,14 @@
+#' @export
+
+relatorio_barao <- function(pais) {
+  rmarkdown::render(system.file("rmd", "comerciobr_report_pais.Rmd", package = "barao"),
+                    params = list(
+                      title = paste0("Brasil-", pais, " , Dados Comerciais"),
+                      pais = pais
+                    ),
+                    # intermediates_dir = "temp",
+                    # output_dir = "output/barao/",
+                    output_file = paste0("comerciobr_", pais, "_",
+                                         barao::comerciobr_get_ulimoano(), "_",
+                                         barao::meses(barao::comerciobr_get_ultimomes())))
+}
