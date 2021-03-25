bb6ef075ef0bfdfe00404fcac36a2c4e10b26f1f
fernandobastosneto <fernando.bastos.neto@gmail.com>
2021-03-24 22:51:28 -0300

    template, testes

--- R/template_itamaraty.R
+++ R/template_itamaraty.R
@@ -1,5 +1,11 @@
 #' Template de relatório para o Itamaraty
 
-template_itamaraty <- function() {
+template_itamaraty <- function(toc = TRUE) {
 
+    # get the locations of resource files located within the package
+    css <- system.file("rmarkdown/templates/template_itamaraty/skeleton/tese.css", package = "barao")
+    template <- system.file("rmarkdown/templates/template_itamaraty/skeleton/template.html", package = "barao")
+
+    # call the base html_document function
+    pagedown::html_paged(css = css, template = template)
 }
