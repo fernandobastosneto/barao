64438d63de2733584b274f7a7323ee25d68791b8
fernandobastosneto <fernando.bastos.neto@gmail.com>
2021-03-24 23:22:58 -0300

    funcao template

--- R/template_itamaraty.R
+++ R/template_itamaraty.R
@@ -1,17 +1,8 @@
 #' Template de relatório para o Itamaraty
 #' @export
-template_itamaraty <- function(toc = TRUE) {
+template_itamaraty <- function(file) {
 
-    # pkg_resource = function(...) {
-    #     system.file(..., package = "barao")
-    # }
-    # css    = pkg_resource("rmarkdown/templates/template_itamaraty/skeleton/tese.css")
-    # header = pkg_resource("rmarkdown/templates/template_itamaraty/skeleton/template.html")
-    #
-    # # get the locations of resource files located within the package
-    # # css <- system.file("rmarkdown/templates/template_itamaraty/skeleton/tese.css", package = "barao")
-    # # template <- system.file("rmarkdown/templates/template_itamaraty/skeleton/template.html", package = "barao")
-    #
-    # # call the base html_document function
-    # pagedown::html_paged(css = css, template = header)
+    arq <- paste0(file,".Rmd")
+
+    rmarkdown::draft(arq, template="template_itamaraty", package = "barao")
 }
