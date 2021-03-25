8791d1362da0430262ab3802160df28a7f333ff3
fernandobastosneto <fernando.bastos.neto@gmail.com>
2021-03-24 23:17:03 -0300

    atualizando template

--- R/template_itamaraty.R
+++ R/template_itamaraty.R
@@ -1,11 +1,17 @@
 #' Template de relatório para o Itamaraty
-
+#' @export
 template_itamaraty <- function(toc = TRUE) {
 
-    # get the locations of resource files located within the package
-    css <- system.file("rmarkdown/templates/template_itamaraty/skeleton/tese.css", package = "barao")
-    template <- system.file("rmarkdown/templates/template_itamaraty/skeleton/template.html", package = "barao")
-
-    # call the base html_document function
-    pagedown::html_paged(css = css, template = template)
+    # pkg_resource = function(...) {
+    #     system.file(..., package = "barao")
+    # }
+    # css    = pkg_resource("rmarkdown/templates/template_itamaraty/skeleton/tese.css")
+    # header = pkg_resource("rmarkdown/templates/template_itamaraty/skeleton/template.html")
+    #
+    # # get the locations of resource files located within the package
+    # # css <- system.file("rmarkdown/templates/template_itamaraty/skeleton/tese.css", package = "barao")
+    # # template <- system.file("rmarkdown/templates/template_itamaraty/skeleton/template.html", package = "barao")
+    #
+    # # call the base html_document function
+    # pagedown::html_paged(css = css, template = header)
 }
