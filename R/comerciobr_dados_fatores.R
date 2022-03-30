99711a1272ea22513e0375c78dc77372fa9c8d86
Núcleo de Inteligência Econômica <101420368+NIEscaec@users.noreply.github.com>
2022-03-30 10:00:13 -0300

    Update comerciobr_dados_fatores.R

--- R/comerciobr_dados_fatores.R
+++ R/comerciobr_dados_fatores.R
@@ -11,20 +11,20 @@
 comerciobr_dados_fatores <- function(pais, periodo, fator) {
 
   if (fator == "isic") {
-    isic <- comerciobr::dic_ncm_isic %>%
+    isic <- comerciobr2::dic_ncm_isic %>%
       dplyr::select(-co_ncm) %>%
       dplyr::distinct()
 
     if (periodo == "mensal") {
 
-    df <- comerciobr::isic_df %>%
+    df <- comerciobr2::isic_df %>%
       dplyr::filter(co_ano == max(co_ano))
 
     }
 
     else {
 
-      df <- comerciobr::isic_df %>%
+      df <- comerciobr2::isic_df %>%
         dplyr::filter(co_ano == max(co_ano)-1)
 
     }
@@ -33,27 +33,27 @@ comerciobr_dados_fatores <- function(pais, periodo, fator) {
       dplyr::group_by(co_isic_secao, co_pais, path) %>%
       dplyr::summarise(value = sum(value)) %>%
       dplyr::rename(co_pais = co_pais) %>%
-      dplyr::left_join(comerciobr::dic_paises, by = "co_pais") %>%
+      dplyr::left_join(comerciobr2::dic_paises, by = "co_pais") %>%
       dplyr::filter(no_pais %in% pais) %>%
       dplyr::left_join(isic)
 
   }
 
   else if (fator == "cuci") {
-    cuci <- comerciobr::dic_ncm_cuci %>%
+    cuci <- comerciobr2::dic_ncm_cuci %>%
       dplyr::select(-co_ncm) %>%
       dplyr::distinct()
 
     if (periodo == "mensal") {
 
-      df <- comerciobr::cuci_df %>%
+      df <- comerciobr2::cuci_df %>%
         dplyr::filter(co_ano == max(co_ano))
 
     }
 
     else {
 
-      df <- comerciobr::cuci_df %>%
+      df <- comerciobr2::cuci_df %>%
         dplyr::filter(co_ano == max(co_ano)-1)
 
     }
@@ -62,7 +62,7 @@ comerciobr_dados_fatores <- function(pais, periodo, fator) {
       dplyr::group_by(co_cuci_sec, co_pais, path) %>%
       dplyr::summarise(value = sum(value)) %>%
       dplyr::rename(co_pais = co_pais) %>%
-      dplyr::left_join(comerciobr::dic_paises, by = "co_pais") %>%
+      dplyr::left_join(comerciobr2::dic_paises, by = "co_pais") %>%
       dplyr::filter(no_pais %in% pais) %>%
       dplyr::left_join(cuci)
 
@@ -70,20 +70,20 @@ comerciobr_dados_fatores <- function(pais, periodo, fator) {
 
   else if (fator == "fator") {
 
-    fator <- comerciobr::dic_ncm_fator %>%
+    fator <- comerciobr2::dic_ncm_fator %>%
       dplyr::select(-co_ncm) %>%
       dplyr::distinct()
 
     if (periodo == "mensal") {
 
-      df <- comerciobr::fator_df %>%
+      df <- comerciobr2::fator_df %>%
         dplyr::filter(co_ano == max(co_ano))
 
     }
 
     else {
 
-      df <- comerciobr::fator_df %>%
+      df <- comerciobr2::fator_df %>%
         dplyr::filter(co_ano == max(co_ano)-1)
 
     }
@@ -92,7 +92,7 @@ comerciobr_dados_fatores <- function(pais, periodo, fator) {
       dplyr::group_by(co_fat_agreg, co_pais, path) %>%
       dplyr::summarise(value = sum(value)) %>%
       dplyr::rename(co_pais = co_pais) %>%
-      dplyr::left_join(comerciobr::dic_paises, by = "co_pais") %>%
+      dplyr::left_join(comerciobr2::dic_paises, by = "co_pais") %>%
       dplyr::filter(no_pais %in% pais) %>%
       dplyr::left_join(fator)
 
@@ -100,21 +100,21 @@ comerciobr_dados_fatores <- function(pais, periodo, fator) {
 
   else if (fator == "cgce") {
 
-    cgce <- comerciobr::dic_ncm_cgce %>%
+    cgce <- comerciobr2::dic_ncm_cgce %>%
       dplyr::select(-co_ncm) %>%
       dplyr::distinct() %>%
       dplyr::mutate(co_cgce_n1 = as.integer(co_cgce_n1))
 
     if (periodo == "mensal") {
 
-      df <- comerciobr::cgce_df %>%
+      df <- comerciobr2::cgce_df %>%
         dplyr::filter(co_ano == max(co_ano))
 
     }
 
     else {
 
-      df <- comerciobr::cgce_df %>%
+      df <- comerciobr2::cgce_df %>%
         dplyr::filter(co_ano == max(co_ano)-1)
 
     }
@@ -123,7 +123,7 @@ comerciobr_dados_fatores <- function(pais, periodo, fator) {
       dplyr::group_by(co_cgce_n1, co_pais, path) %>%
       dplyr::summarise(value = sum(value)) %>%
       dplyr::rename(co_pais = co_pais) %>%
-      dplyr::left_join(comerciobr::dic_paises, by = "co_pais") %>%
+      dplyr::left_join(comerciobr2::dic_paises, by = "co_pais") %>%
       dplyr::filter(no_pais %in% pais) %>%
       dplyr::left_join(cgce, by = "co_cgce_n1")
 
