03911492997c2a32acd2f21c9240f9e1d6d1bc39
Núcleo de Inteligência Econômica <101420368+NIEscaec@users.noreply.github.com>
2022-03-30 10:15:05 -0300

    Update comerciobr_get_ultimoano.R

--- R/comerciobr_get_ultimoano.R
+++ R/comerciobr_get_ultimoano.R
@@ -5,7 +5,7 @@
 #' @export
 comerciobr_get_ulimoano <- function() {
 
-  comerciobr::sh1_df %>%
+  comerciobr2::sh1_df %>%
     dplyr::filter(co_ano == max(co_ano)) %>%
     dplyr::distinct(co_ano) %>%
     dplyr::pull(co_ano)
