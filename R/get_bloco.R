05fd1423d50ebbea2091c223c5d96de0b816bb96
cmarconijr <67400736+cmarconijr@users.noreply.github.com>
2022-03-31 09:07:59 -0300

    Update get_bloco.R

--- R/get_bloco.R
+++ R/get_bloco.R
@@ -7,7 +7,7 @@
 
 get_bloco <- function(paises) {
 
-  bloco <- comerciobr::dic_blocos %>%
+  bloco <- comerciobr2::dic_blocos %>%
     dplyr::filter(no_pais %in% paises) %>%
     dplyr::distinct(no_bloco) %>%
     dplyr::pull(no_bloco)
