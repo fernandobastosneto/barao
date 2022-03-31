23d77fedfe28084d9033771ad4bcd388a314a778
cmarconijr <67400736+cmarconijr@users.noreply.github.com>
2022-03-31 14:38:47 -0300

    Update test-comerciomundo_grafico_produtos_proporcao.R

--- tests/testthat/test-comerciomundo_grafico_produtos_proporcao.R
+++ tests/testthat/test-comerciomundo_grafico_produtos_proporcao.R
@@ -1,4 +1,4 @@
 test_that("comerciomundo grafico produtos proporcao funciona", {
-  grafico <- barao::comerciomundo_grafico_produtos_proporcao("Portugal")
+  grafico <- barao2::comerciomundo_grafico_produtos_proporcao("Portugal")
   expect_type(grafico, "list")
 })
